ad6201a881e42bec0bcc3dbd4af1165c995596ea
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2023-05-30 20:15:14 +0200

    Add hover effect and changeable size
    This commit adds hover effect that changes color of grid squares.
    It also adds ability to change grid size with input.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,37 @@
+let size = 16;
+const button = document.querySelector('button');
+
 function makeGrid() {
     const container = document.querySelector('.container');
     const grid = document.createElement('div');
-    for (let i = 0; i < 16; i++) {
+    container.innerHTML = '';
+    for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
+            box.addEventListener("mouseover", changeColor);
             row.appendChild(box);
         }
         container.appendChild(row);
     }
 }
 
+function changeColor() {
+    this.classList.add('changeColor');
+}
+
+function getSize(event) {
+    event.preventDefault();
+    size = document.querySelector('input[name="size"]').value;
+    if (size < 2 || size > 100) {
+        alert("Pick size between 2 and 100.");
+    } else {
+        makeGrid();
+    }
+}
+
 makeGrid();
+
+button.addEventListener("click", getSize);
